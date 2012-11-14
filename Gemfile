ff2316cc5bb708ea03af143585bea0f0183b0038
Adam Boas <adam.boas@gmail.com>
2012-11-14 22:36:55 +1100

    init

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'pg'
+gem 'jquery-rails'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'bootstrap-sass'
+  gem 'font-awesome-sass-rails'
+
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  # gem 'therubyracer', :platforms => :ruby
+
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TinyRobotArmy::Application.config.secret_token = '3fd01523a7cac107bd1ad87b64863b297a068fc21de5c2656666cedd53917f7239d8ffd0a87f4f4d3958a222f4d5b8fa0ea1b18748497bfced0127f15f619cb5'

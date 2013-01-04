657ad8e5f6f1662682b9c539599d4f1c14575a97
Adam Boas <adam.boas@gmail.com>
2013-01-04 16:34:28 +1100

    pended the import spec for rss_import, not sure whether to proceed with this right now

--- spec/lib/rss_import_spec.rb
+++ spec/lib/rss_import_spec.rb
@@ -1,15 +1,20 @@
 require "spec_helper"
 
 describe RssImport do
-  let(:rss_import) { RssImport.new('http://www.oneadam.net/index.html?feed=rss2') }
-  it 'should load from a file name' do
-    rss_import.should be_feed_loaded
+  describe 'a newly created rss_import' do
+    let(:url) { 'http://www.oneadam.net/index.html?feed=rss2' }
+    before do
+      Feedzirra::Feed.stub(:fetch_and_parse).and_return 'parsed feed'
+    end
+
+    it 'should fetch and parse url when initialized' do
+      Feedzirra::Feed.should_receive(:fetch_and_parse).with(url)
+      RssImport.new(url)
+    end
   end
 
   describe '#import' do
-    it 'should do an import' do
-      rss_import.import(User.create(email: 'test@test.com', password: 'password'))
-      puts "Count: #{Post.all.count}"
+    pending 'should do an import' do
     end
   end
 end

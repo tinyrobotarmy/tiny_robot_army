c24cde5009ec596c29cc3987087b7032d0d582ff
Adam Boas <adam.boas@gmail.com>
2013-04-20 10:11:05 +1000

    Added restful routes for admin posts

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,20 +1,38 @@
 class Admin::PostsController < Admin::AdminController
   respond_to :html
 
+
   def index
-    respond_with @posts = Post.order('created_at DESC')
+    respond_with :admin, @posts = Post.order('created_at DESC')
   end
 
   def show
     @new_comment = Comment.new()
-    respond_with @post = Post.find(params[:id])
+    respond_with :admin, @post = Post.find(params[:id])
   end
 
   def new
-    respond_with @post = Post.new, layout: !request.xhr?
+    respond_with :admin, @post = Post.new, layout: !request.xhr?
   end
 
   def create
-    respond_with @post = Post.create(params[:post].merge(author: current_user))
+    respond_with :admin, @post = Post.create(params[:post].merge(author: current_user))
   end
+
+  def edit
+    respond_with :admin, @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(params[:post])
+    respond_with :admin, @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    respond_with(:admin, @post)
+  end
+
 end
--- spec/controllers/admin/posts_controller_spec.rb
+++ spec/controllers/admin/posts_controller_spec.rb
@@ -7,12 +7,21 @@ describe Admin::PostsController do
     controller.stub(:current_user).and_return stub_model(User)
   end
 
+  def valid_attributes(add_or_override={})
+    {subject: 'good one', body: 'great bod'}.merge(add_or_override)
+  end
+
   describe 'GET #new' do
     subject { get :new }
 
     it 'should expose a new Post as @post for editing' do
       subject
       assigns(:post).should be_new_record
+    end
+
+    it 'should render the new template' do
+      subject
+      response.should render_template :new
     end
   end
 
@@ -22,16 +31,56 @@ describe Admin::PostsController do
     context 'when attributes are invalid' do
       let(:post_attributes) { {subject: 'awesome post'} }
       it 'should re-render the new template' do
-        subject 
+        subject
         response.should render_template(:new)
       end
     end
 
     context 'when attributes are valid' do
-      let(:post_attributes) { {subject: 'good one', body: 'great bod'} }
+      let(:post_attributes) { valid_attributes }
       it 'should be a success' do
         subject
-        response.should redirect_to post_path(assigns(:post))
+        response.should redirect_to admin_post_path(assigns(:post))
+      end
+    end
+  end
+
+  describe 'GET #edit' do
+    subject { get :edit, id: post.id }
+    let(:post) { Post.create!(valid_attributes)}
+    it 'should make the post available for editing' do
+      subject
+      assigns(:post).should == post
+    end
+
+    it 'should render the edit template' do
+      subject
+      response.should render_template :edit
+    end
+  end
+
+  describe 'PUT #update' do
+    subject { put :update, id: post.id, post: attributes }
+    let(:post) { Post.create!(valid_attributes)}
+    context 'with valid attributes' do
+      let(:attributes) { valid_attributes(subject: 'new subject') }
+
+      it 'should update the post' do
+        subject
+        post.reload.subject.should == 'new subject'
+      end
+
+      it 'should redirect to the post show' do
+        subject
+        response.should redirect_to admin_post_path(post)
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:attributes) { {subject: ''} }
+      it 'should render the edit template' do
+        subject
+        response.should render_template :edit
       end
     end
   end

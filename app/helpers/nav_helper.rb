a6f1e86ee453b2439c895e6227f8a127e58d5f66
Adam Boas <adam.boas@gmail.com>
2013-04-22 22:08:59 +1000

    Moved admin_helper to nav_helper and moved all nav link labels into it

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -1,4 +1,16 @@
-module AdminHelper
+module NavHelper
+  def home_link_label
+    content_tag(:i, '', class: 'icon-home') + content_tag(:div, t('.home'))
+  end
+
+  def contact_link_label
+    content_tag(:i, '', class: 'icon-envelope') + content_tag(:div, t('.contact_us'))
+  end
+
+  def blog_link_label
+    content_tag(:i, '', class: 'icon-rss') + content_tag(:div, t('.blog'))
+  end
+
   def site_link_label
     content_tag(:i, '', class: 'icon-globe') + content_tag(:div, t('.site'))
   end
